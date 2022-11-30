5c3698b316bac80d9f5cd39c49a5045c412638b0
farihaNprithila <farihanprithila@gmail.com>
2022-12-01 01:08:43 +0600

    Adding route

---
+++ src/routes/routes.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/src/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/src/service"
+)
+
+var (
+	userService    service.UserService       = service.New()
+	userController controller.UserController = controller.New(userService)
+)
+
+func Routes() {
+	route := gin.Default()
+	route.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.FindAll())
+
+	})
+	route.POST("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.Save(ctx))
+	})
+
+	err := route.Run(":8080")
+	if err != nil {
+		return
+	}
+}
