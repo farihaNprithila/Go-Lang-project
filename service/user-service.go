66ee8afa890b21fa14f81ac86b676b628c92f4d7
fariha-bs1026 <fariha.nawaz@brainstation-23.com>
2022-11-30 21:05:44 +0600

    Creating service

---
+++ service/user-service.go
@@ -0,0 +1,25 @@
+package service
+
+import "gitlab.com/pragmaticreviews/golang-gin-poc/entity"
+
+type UserService interface {
+	Save(user entity.User) entity.User
+	FindAll() []entity.User
+}
+
+type userService struct {
+	users []entity.User
+}
+
+func New() UserService {
+	return &userService{}
+}
+
+func (service *userService) Save(user entity.User) entity.User {
+	service.users = append(service.users)
+	return user
+}
+
+func (service *userService) FindAll() []entity.User {
+	return service.users
+}
