ab0694d7a1a51f8ded426853ad9197a3304f7d02
farihaNprithila <farihanprithila@gmail.com>
2022-12-01 01:09:44 +0600

    Code refactoring

--- server.go
+++ server.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
-	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
-)
-
-var (
-	userService    service.UserService       = service.New()
-	userController controller.UserController = controller.New(userService)
+	"gitlab.com/pragmaticreviews/golang-gin-poc/src/routes"
 )
 
 func main() {
-	server := gin.Default()
-	server.GET("/users", func(ctx *gin.Context) {
-		ctx.JSON(200, userController.FindAll())
-
-	})
-	server.POST("/users", func(ctx *gin.Context) {
-		ctx.JSON(200, userController.Save(ctx))
-	})
-
-	server.Run(":8080")
+	routes.Routes()
 }
