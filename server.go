5812bd1f02a17a177a21b5272176b576b3db91ab
fariha-bs1026 <fariha.nawaz@brainstation-23.com>
2022-11-30 22:22:45 +0600

    Refactoring code for getting user

--- server.go
+++ server.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
+)
+
+var (
+	userService    service.UserService       = service.New()
+	userController controller.UserController = controller.New(userService)
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello this is ping",
-		})
+	server.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.FindAll())
+
+	})
+	server.POST("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, userController.Save(ctx))
 	})
 
 	server.Run(":8080")
