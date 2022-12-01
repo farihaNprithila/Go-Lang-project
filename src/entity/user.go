a65d0789eaadf4188a599f3d3aa8532a5993314d
farihaNprithila <farihanprithila@gmail.com>
2022-12-02 01:05:54 +0600

    Creating entity for gorm

--- src/entity/user.go
+++ src/entity/user.go
@@ -1,6 +1,11 @@
 package entity
 
 type User struct {
-	UserName string `json:"username" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
+	ID        uint64 `json:"id" gorm:"primary_key:auto increment"`
+	UserName  string `json:"username" binding:"required" gorm:"type:varchar(30)"`
+	Password  string `json:"password" binding:"required" gorm:"->;<-;not null"`
+	FirstName string `json:"firstName" gorm:"type:varchar(40)"`
+	LastName  string `json:"lastName" gorm:"type:varchar(40)"`
+	Email     string `json:"email" binding:"required,email" gorm:"type:varchar(50)"`
+	Token     string `json:"token.omitempty" gorm:"-"`
 }
